5c7634d2cb2018700ea17beb1a48a7e3d2154b62
atoghyani <ariantoghyani@gmail.com>
2018-09-18 09:55:48 -0500

    Added Trees And chests

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,21 +9,32 @@ public class Player : NetworkBehaviour
 
     Rigidbody2D myRigidBody;
     Animator myAnimator;
+    SpriteRenderer mySpriteRenderer;
     Collider2D myCollider;
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpHeight = 15f;
-    
-    
-    void Start ()
+    bool isGrounded=true;
+
+    Transform currentItem = null;
+    Score scoreLeft;
+    Score scoreRight;
+
+
+    void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         myCollider = GetComponent<Collider2D>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        scoreLeft = GameObject.FindWithTag("Chest Left").GetComponent<Score>();
+        scoreRight = GameObject.FindWithTag("Chest Right").GetComponent<Score>();
+        Vector2 cl = new Vector2(transform.position.x, transform.position.y + 1);
+
 
     }
-    
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
         if (!isLocalPlayer)
         {
@@ -33,31 +44,33 @@ public class Player : NetworkBehaviour
         Run();
         CmdFlipSprite();
         Jump();
+        Vector2 cl = new Vector2(transform.position.x, transform.position.y+1);
+    
 
-       
+
     }/// <summary>
      /// khcghadgfkja
      /// </summary>
      /// 
 
-  
+
     public void CmdFlipSprite()
     {
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
         myAnimator.SetBool("Running", playerHasHorizontalSpeed);
         FlipSprite();
     }
-    
+
 
 
     private void Run()
     {
         //    transfrom
         float controlThrow = Input.GetAxis("Horizontal");
-        Vector2 playerVelocity = new Vector2(controlThrow*runSpeed, myRigidBody.velocity.y);
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
 
-        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon && myRigidBody.velocity.y == 0;
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon && myRigidBody.velocity.y == 0f;
         myAnimator.SetBool("Running", playerHasHorizontalSpeed);
 
 
@@ -65,7 +78,7 @@ public class Player : NetworkBehaviour
     private void FlipSprite()
     {
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
-        if(playerHasHorizontalSpeed)
+        if (playerHasHorizontalSpeed)
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
         }
@@ -74,17 +87,61 @@ public class Player : NetworkBehaviour
 
     private void Jump()
     {
-       // if (!myCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        //if (!myCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
 
-            if (Input.GetButtonDown("Jump")&&myRigidBody.velocity.y==0)
-            {
-                
-                Vector2 playerVelocity = new Vector2(0f, jumpHeight);
-           
-                myRigidBody.velocity += playerVelocity;
-           
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+
+            Vector2 playerVelocity = new Vector2(0f, jumpHeight);
+
+            myRigidBody.velocity += playerVelocity;
+
+
 
         }
+
+    }
+
+
+
+    void OnCollisionEnter2D(Collision2D Other)
+    {
+       
+            isGrounded = true;
+            // canjump = true;
+            Debug.Log("COLL-Enter" + "  -  Ground:" + isGrounded);
+       
+    }
+
+    void OnCollisionExit2D(Collision2D Other)
+    {
+       
+            isGrounded = false;
+
+            Debug.Log("COLL-Exit" + "  -  Ground:" + isGrounded);
         
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // pickup if it has tag "Item" and we are not carrying anything
+        if (other.CompareTag("Item") && currentItem == null)
+        {
+            mySpriteRenderer.color = Color.blue;
+        }
+        if (other.CompareTag("Chest Right") && currentItem == null)
+        {
+            mySpriteRenderer.color = Color.white;
+            Debug.Log("GOAL");
+            scoreRight.ReduceScore();
+
+        }
+        if (other.CompareTag("Chest Left") && currentItem == null)
+        {
+            mySpriteRenderer.color = Color.white;
+            Debug.Log("GOAL");
+            scoreLeft.ReduceScore();
+
+        }
     }
 }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour {
+
+    [SerializeField]TextMeshProUGUI scoreText;
+    // Use this for initialization
+    int score = 5;
+   
+    
+	void Start () {
+
+        scoreText.text = score.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public void ReduceScore()
+    {
+        score--;
+    }
+}
